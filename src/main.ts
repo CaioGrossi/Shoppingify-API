3821f8d46f478600168182eef8931e708da9ed99
CaioGrossi <caiogrossi6@gmail.com>
2021-05-14 20:46:09 -0300

    Added dotenv to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
